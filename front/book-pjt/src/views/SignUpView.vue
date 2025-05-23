0cb928fe4c81583bd02157824c1c921a50e0cff1
이승원 <seungwon986@gmail.com>
2025-05-23 09:24:29 +0900

    account_update

--- front/book-pjt/src/views/SignUpView.vue
+++ front/book-pjt/src/views/SignUpView.vue
@@ -1,29 +1,53 @@
 <template>
-  <div class="container mt-5">
-    <h2 class="mb-4">회원가입</h2>
-    <component
-      :is="steps[currentStep]"
-      v-model:user="user"
-      v-model:selectedCategories="selectedCategories"
-      v-model:selectedBooks="selectedBooks"
-      @next="currentStep++"
-      @prev="currentStep--"
-    />
+  <div class="container py-4">
+    <div class="card p-4 shadow">
+      <h2 class="mb-4">회원가입</h2>
+
+      <component
+        :is="steps[currentStep]"
+        :form="form"
+        @next="nextStep"
+        @prev="prevStep"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+
+
 import SignUpStep1 from '@/components/signup/SignUpStep1.vue'
 import SignUpStep2 from '@/components/signup/SignUpStep2.vue'
 import SignUpStep3 from '@/components/signup/SignUpStep3.vue'
 import SignUpStep4 from '@/components/signup/SignUpStep4.vue'
 
 
+const form = ref({
+  username: '',
+  password1: '',
+  nickname: '',
+  gender: '',
+  birth: '',
+  preferred_categories: [],
+  preferred_books: [],
+})
+
+
 const currentStep = ref(0)
 const steps = [SignUpStep1, SignUpStep2, SignUpStep3, SignUpStep4]
 
-const user = ref({})
-const selectedCategories = ref([])
-const selectedBooks = ref([])
+const nextStep = () => {
+  if (currentStep.value < steps.length - 1) currentStep.value++
+}
+
+const prevStep = () => {
+  if (currentStep.value > 0) currentStep.value--
+}
 </script>
+
+<style scoped>
+.container {
+  max-width: 700px;
+}
+</style>
