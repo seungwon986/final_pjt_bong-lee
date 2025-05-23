376024384eb50d89a2750066eccc0ff8c7b43ee2
bonghyerin <votreregard@naver.com>
2025-05-23 22:16:51 +0900

    도서목록디자인변경

--- back/book_pjt/books/serializers.py
+++ back/book_pjt/books/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import Book, Category
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
-
+# 카테고리 직렬화
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']
+
+# 책 직렬화 (category를 CategorySerializer로 중첩)
+class BookSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)  # ⭐️ 요기!
+
+    class Meta:
+        model = Book
+        fields = '__all__'
